034aaf5970c2556c288236dc534a579eee0d3a72
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-06-17 10:04:21 -0300

    ci(Created stacks script): :hammer:

---
+++ src/scripts/stacks.ts
@@ -0,0 +1,53 @@
+import bootstrap from "../assets/stacks-icon/bootstrap.svg";
+import css3 from "../assets/stacks-icon/css3.svg";
+import git from "../assets/stacks-icon/git.svg";
+import github from "../assets/stacks-icon/github.svg";
+import html5 from "../assets/stacks-icon/html5.svg";
+import javascript from "../assets/stacks-icon/javascript.svg";
+import react from "../assets/stacks-icon/react.svg";
+import typescript from "../assets/stacks-icon/typescript.svg";
+
+const stacks = [
+  {
+    id: 1,
+    name: "html",
+    icon: html5,
+  },
+  {
+    id: 2,
+    name: "css",
+    icon: css3,
+  },
+  {
+    id: 3,
+    name: "javascript",
+    icon: javascript,
+  },
+  {
+    id: 4,
+    name: "typescript",
+    icon: typescript,
+  },
+  {
+    id: 5,
+    name: "react",
+    icon: react,
+  },
+  {
+    id: 6,
+    name: "bootstrap",
+    icon: bootstrap,
+  },
+  {
+    id: 7,
+    name: "git",
+    icon: git,
+  },
+  {
+    id: 8,
+    name: "github",
+    icon: github,
+  },
+];
+
+export default stacks;
